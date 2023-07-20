065227084255a8c36aba2bd605b40112deb2b6d9
yakkanti-rami-reddy <ramyakkanti143@gmail.com>
2023-07-20 18:30:16 +0000

    COMMIT 1

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup"
+import Customerapplyloan from "./components/Customerapplyloan/Customerapplyloan";
+import "./components/Signup/Signup.css"
+import './components/Login/Login.css'
+import HomePage from "./components/HomePage/HomePage";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Customerloanstatus from "./components/Customerloanstatus/Customerloanstatus";
+import Customerprofile from "./components/Customerprofile/Customerprofile";
+import AdminPage from "./components/AdminPage/admin"
+import Adminappliedloans from "./components/Adminappliedloans/Adminappliedloans";
+import Loanid from "./components/Loanid/Loanid";
+import './components/Loanid/Loanid.css'
+import Adminloandetails from "./components/Adminloandetails/Adminloandetails";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+          <Route path='/home' element={<HomePage />}></Route>
+          <Route path='/admin' element={<AdminPage />}></Route>
+          <Route path='/Applyloan' element={<Customerapplyloan />}></Route>
+          <Route path='/loanstatus' element={<Customerloanstatus />}></Route>
+          <Route path='/profile' element={<Customerprofile />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/Signup' element={<Signup />}></Route>
+          <Route path='/Appliedloans' element={<Adminappliedloans />}></Route>
+          <Route path='/LoanDetails' element={<Adminloandetails />}></Route>
+          <Route path='/Loanid' element={<Loanid />}></Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
